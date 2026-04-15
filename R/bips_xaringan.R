c35e189c67df74f83a3a013c6dbe640840598a9e
Lukas Burk <lukas@quantenbrot.de>
2026-04-15 19:07:45 +0200

    Overhaul to use 'proper' rmarkdown output formats

--- R/bips_xaringan.R
+++ R/bips_xaringan.R
@@ -1,5 +1,13 @@
+#' BIPS xaringan presentation format
+#'
+#' Wrapper around [xaringan::moon_reader()] with BIPS styling defaults.
+#'
+#' @inheritParams xaringan::moon_reader
+#' @param css CSS files to use. Defaults to BIPS theme and xaringan defaults.
+#' @param ... Additional arguments passed to [xaringan::moon_reader()].
+#' @export
 bips_xaringan <- function(
-  css = c("bips", "default"),
+  css = NULL,
   self_contained = FALSE,
   seal = TRUE, yolo = FALSE,
   chakra = "https://remarkjs.com/downloads/remark-latest.min.js",
@@ -13,16 +21,21 @@ bips_xaringan <- function(
   anchor_sections = FALSE, ...
 ) {
 
-  pkg_resource("xaringan", "bips.css")
+  bips_css <- pkg_resource("xaringan", "bips.css")
+
+  if (is.null(css)) {
+    css <- c(bips_css, "default")
+  }
 
   xaringan::moon_reader(
     css = css, self_contained = self_contained,
     seal = seal, yolo = yolo, chakra = chakra,
-    nature = nature, anchor_sections = anchor_sections
+    nature = nature, anchor_sections = anchor_sections,
+    ...
   )
 }
 
-pkg_resource <- function(template,...) {
+pkg_resource <- function(template, ...) {
   system.file("rmarkdown", "templates", template, "resources",
               ..., package = "bipsdown", mustWork = TRUE)
 }
